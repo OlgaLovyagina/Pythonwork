ef1cd6a71ed00a935a78f6c0ca6f67dc9495d871
Olga <ol_0524@mail.ru>
2022-12-23 18:18:38 +0300

    Домашняя работа 8

--- dz8/view.py
+++ dz8/view.py
@@ -3,24 +3,29 @@
 
 def choose() -> str:
     """"Выбор режима работы приложения"""
-    pass
-
+    print('1 - решить пример\n\
+    2- найти корни уравнения\n\
+    3 - упростить многочлен\n\
+    4 - показать историю')
+    return input('выбран пункт: ')
 
 def get_expr() -> str:
     """"Запрашивает у пользователя задачу"""
-    pass
+    return input('введите задачу: ')
 
 
 def show_res(res: str):
     """Выводит результат"""
-    pass
+    print(res)
 
 
 def erorr_mode():
     """Выводит сообщение об ошибке выбора режима"""
-    pass
+    print('такого режима нет')
 
 
 def show_history(history: str):
     """Выводит истроию оперций"""
-    pass
+    print(history.split('\n'))
+    for i in history.split('\n'):
+        print(i.replace(';', ' -> result: '))
